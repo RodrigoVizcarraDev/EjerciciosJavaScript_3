7455b4f9a6209ed8b2926eb66c15b69b669ba8d5
Rodrigo Vizcarra <rodrigovizcarra1996@gmail.com>
2023-03-16 01:31:11 -0300

    Ejercicio 2 completado. Añadiendo ultimos requisitos

--- JS/ejercicio2.js
+++ JS/ejercicio2.js
@@ -22,38 +22,60 @@ let variableControl = true;
 const ciudades = [];
 let ciudadesAgregadas;
 
-do{
+do {
     alert("Ingrese la ciudad que quiere agregar a la lista");
     ciudadesAgregadas = prompt("Ingrese la ciudad");
 
     // Comprobando si el usuario pulsa cancelar sin ingresar valor
-    if(ciudadesAgregadas === null){
+    if (ciudadesAgregadas === null) {
         alert("No ingreso nada");
         variableControl = confirm("Cancelar si quiere salir | Aceptar si quiere volver a intentar");
 
-        if(variableControl === false){
+        if (variableControl === false) {
             alert("Saliendo del programa");
             break;
-        }else{
+        } else {
             continue;
         }
     }
 
     // Comprobando si el usuario ingreso un texto o un numero
-    if(!isNaN(ciudadesAgregadas)){
+    if (!isNaN(ciudadesAgregadas)) {
         alert("Ingreso un numero");
         variableControl = confirm("Quiere volver a intentar o salir?");
         continue;
-    }else{
+    } else {
         ciudades.push(ciudadesAgregadas);
     }
-    
+
     variableControl = confirm("Quiere agregar otra ciudad o salir|ACEPTAR O CANCELAR|?");
 
-}while(variableControl)
+} while (variableControl)
 
-if(ciudades.length === 0){
+if (ciudades.length === 0) {
     document.write(`<h3>No agrego ninguna ciudad</h3>`);
-}else{
+} else {
+    // Añadiendo en ultima posicion la ciudad de paris
+    ciudades.push("Paris");
+
     document.write(`<h3>La lista de ciudades es: ${JSON.stringify(ciudades)}</h3>`);
+
+    //Mostrando longitud del arreglo
+    document.write(`La longitud del arreglo es: ${ciudades.length}`);
+
+    // Escribe por pantalla el elemento que ocupa la segunda posición.
+    document.write(`Elemento posicion 2: ${ciudades[1]}`);
+
+    // Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+    document.write(`Sustituiremos ${ciudades[1]} por Barcelona`);
+    ciudades[1] == "Barcelona";
+    
+    if (ciudades.length >= 4) {
+
+        // Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+        document.write(`Posicion 1: ${ciudades[0]} <br>
+                        Posicion 3: ${ciudades[2]} <br>
+                        Ultima posicion: ${ciudades[ciudades.length - 1]}`);
+    }
+
 }
